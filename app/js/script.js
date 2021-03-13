a1bf80df4640bf6413373e2ed8c9974a2c4b3212
Massimo Biancalani <massimo.biancalani@gmail.com>
2021-03-13 20:18:19 +0100

    Animated mobile menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,16 +1,22 @@
 const headerMenu = document.getElementById('btnHamburger');
+const body = document.body;
 const header = document.querySelector('.header');
-const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
 
 headerMenu.addEventListener('click', function(){ 
   if (header.classList.contains('open')) {
     header.classList.remove('open');
-    overlay.classList.remove('fade-in');
-
-    overlay.classList.add('fade-out');
+    body.classList.remove('noscroll');
+    fadeElems.forEach(elem => {
+      elem.classList.remove('fade-in');
+      elem.classList.add('fade-out');
+    });
   } else {
       header.classList.add('open');
-      overlay.classList.remove('fade-out');
-      overlay.classList.add('fade-in');
+      body.classList.add('noscroll');
+      fadeElems.forEach(elem => {
+        elem.classList.add('fade-in');
+        elem.classList.remove('fade-out');
+      });
   }
 }); 
